00b0fddb4d9f8274a33b09e8ffd0500fef688ed9
sileskyd <sileskyd@gmail.com>
2023-04-20 13:27:36 -0600

    Header y el Formulario

--- ProyectoFinal/src/App.jsx
+++ ProyectoFinal/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './header'
+import Form from './form'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
+      <Header/>
+      <Form/>
       
     </div>
   )
---
+++ ProyectoFinal/src/Form.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Form() {
+  return (
+    <div>
+      <h2>Form Works!</h2>
+    </div>
+  )
+}
