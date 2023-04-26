fd79ec7c00329c928c0a378cb519aa554d1d8b5a
sileskyd <sileskyd@gmail.com>
2023-04-25 18:03:01 -0600

    Cambios en formulario
    Se agregó labels, inputs y useState para las mascotas.

--- ProyectoFinal/src/Form.jsx
+++ ProyectoFinal/src/Form.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './form.css'
 
 export default function Form() {
+
+  const[mascota,setMascota]=useState("Perro")
+
   return (
     <div>
-      <h2>Form Works!</h2>
+      <div>
+        <form className='inputForm' action=''>
+          <label htmlFor=''>Nombre Mascota</label>
+          <input onChange={(e)=>{setMascota(e.target.value)}} value={mascota} type='text' />
+          <label htmlFor=''>Nombre Dueño</label>
+          <input type='text' />
+          <button type='submit'>Agregar</button>
+        </form>
+      </div>
     </div>
   )
 }
